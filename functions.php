7400ef2470e7178c2485f138db9d90d7a51bc74a
plumvillage <om@plumvillage.org>
2012-12-06 20:11:57 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,3 +13,11 @@ require ( get_template_directory() . '/includes/functions.php' );
 require ( get_template_directory() . '/includes/theme-options.php' );
 require ( get_template_directory() . '/includes/hooks.php' );
 require ( get_template_directory() . '/includes/version.php' );
+
+//jQuery Insert From Google
+if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+function my_jquery_enqueue() {
+   wp_deregister_script('jquery');
+   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", false, null);
+   wp_enqueue_script('jquery');
+}
